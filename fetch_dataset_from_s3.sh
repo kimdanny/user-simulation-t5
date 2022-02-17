04d6ed64704d1044ab01caa47d082d1bc72c3459
EC2 Default User <ec2-user@ip-172-31-66-22.ec2.internal>
2022-02-17 01:30:59 +0000

    dataset fetch from s3

---
+++ fetch_dataset_from_s3.sh
@@ -0,0 +1,4 @@
+aws s3 cp s3://user-simulation-dataset/act-sat/ ./dataset/act-sat/ --recursive
+aws s3 cp s3://user-simulation-dataset/act-sat-utt/ ./dataset/act-sat-utt/ --recursive
+aws s3 cp s3://user-simulation-dataset/act-sat_no-alt/ ./dataset/act-sat_no-alt/ --recursive
+aws s3 cp s3://user-simulation-dataset/act-sat-utt_no-alt/ ./dataset/act-sat-utt_no-alt/ --recursive
