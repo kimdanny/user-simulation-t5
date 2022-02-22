ff55534c04859d9334c2b7eb6d29ae78e506fa05
EC2 Default User <dannykim153@gmail.com>
2022-02-22 02:26:48 +0000

    utt single task model fetch from s3

--- fetch_all_models_from_s3.sh
+++ fetch_all_models_from_s3.sh
@@ -10,3 +10,5 @@ aws s3 cp s3://user-simulation-t5-models/act-sat_no-alt/CCPE/ simpleT5/act-sat_n
 aws s3 cp s3://user-simulation-t5-models/act-sat-utt_no-alt/MWOZ/ simpleT5/act-sat-utt_no-alt/MWOZ/ --recursive
 aws s3 cp s3://user-simulation-t5-models/act-sat-utt_no-alt/SGD/ simpleT5/act-sat-utt_no-alt/SGD/ --recursive
 aws s3 cp s3://user-simulation-t5-models/act-sat-utt_no-alt/CCPE/ simpleT5/act-sat-utt_no-alt/CCPE/ --recursive
+
+aws s3 cp s3://user-simulation-t5-models/utt_no-alt/ simpleT5/utt_no-alt/ --recursive
